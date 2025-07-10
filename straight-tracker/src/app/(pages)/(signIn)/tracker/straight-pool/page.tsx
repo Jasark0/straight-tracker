b65b447cf70fdef531155dc4399c7f5b3e7aeda8
jason <jasonyen2004@gmail.com>
2025-07-10 13:45:42 -0500

    feat: straight pool tracker get match API created. Pulling data normally into individual matches.

--- straight-tracker/src/app/(pages)/(signIn)/tracker/straight-pool/page.tsx
+++ straight-tracker/src/app/(pages)/(signIn)/tracker/straight-pool/page.tsx
@@ -2,37 +2,106 @@
 
 import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
+import { useSearchParams } from 'next/navigation';
 
 import Header from '@/src/components/Header';
 
 const Tracker: React.FC = () => {
-    return (
-        <div className="s-main-box">
-            <Header></Header>
-            
-            <div className="remaining-balls-container">
-                <p className="remaining-balls-style remaining-balls">
-                    
-                    15
-                </p>
-                <p className="remaining-balls-style">
-                    Remaining Balls
-                </p>
-                <p className="rack-number">
-                    (Rack 1)
-                </p>
+    const router = useRouter();
+
+    const searchParams = useSearchParams();
+    const matchID = searchParams.get('matchID');
+    const [gameName, setGameName] = useState('');
+    const [player1, setPlayer1] = useState('');
+    const [player2, setPlayer2] = useState('');
+    const [raceTo, setRaceTo] = useState('5');
+    const [toShoot, setToShoot] = useState('');
+    const [rack, setRack] = useState<number>(1);
+    const [remainingBalls, setRemainingBalls] = useState<number>(15);
+    const [player1Score, setPlayer1Score] = useState<number>(0);
+    const [player1HighRun, setPlayer1HighRun] = useState<number>(0);
+    const [player1CurrRun, setPlayer1CurrRun] = useState<number>(0);
+    const [player2Score, setPlayer2Score] = useState<number>(0);
+    const [player2HighRun, setPlayer2HighRun] = useState<number>(0);
+    const [player2CurrRun, setPlayer2CurrRun] = useState<number>(0);
+    
+    const [winner, setWinner] = useState('');
+
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+    
+    useEffect(() => { //Get match info
+        const fetchMatch = async () => {
+            try{
+                const res = await fetch(`/api/getStraightMatch?matchID=${matchID}`);
+                const json = await res.json();
+                
+                if (json.redirect) {
+                    router.push(json.redirect);
+                    return;
+                }
+
+                console.log(json.straightMatch);
+                setGameName(json.straightMatch.game_name);
+                setPlayer1(json.straightMatch.player1);
+                setPlayer2(json.straightMatch.player2);
+                setRaceTo(json.straightMatch.race_to);
+                setToShoot(json.straightMatch.to_shoot);
+                setRack(json.straightMatch.rack);
+                setRemainingBalls(json.straightMatch.remaining_balls);
+                setPlayer1Score(json.straightMatch.player1_score);
+                setPlayer1HighRun(json.straightMatch.player1_high_run);
+                setPlayer1CurrRun(json.straightMatch.player1_curr_run);
+                setPlayer2Score(json.straightMatch.player2_score);
+                setPlayer2HighRun(json.straightMatch.player2_high_run);
+                setPlayer2CurrRun(json.straightMatch.player2_curr_run)
+            }
+            catch (err){
+                setError('Error');
+            }
+            finally{
+                setLoading(false);
+            };
+        }
+        fetchMatch();
+    }, [matchID]);
+
+    if (loading){ //Loading screen
+        return (
+            <div className="page-box">
+                <div className="loading-screen">
+                    <Header/>
+                    <div className="loading-content">
+                        <p>Loading match info...</p>
+                        <img src="/spinner.gif" className="spinner-css" alt="Loading..."></img>
+                    </div>
+                </div>
             </div>
-            
-            <div className="s-player-boxes">
-                <div className="s-player1-box">
-                    <p id = "player1" className="s-player1-name">Jason</p>
-                    <div className="s-score-box">
+        );
+    }
+
+    return (
+        <div className="s-main-container">
+            <Header></Header>
+            <p className="s-game-name-text">
+                Game Name: {gameName}
+            </p>
+            <p className="s-race-to-text">
+                Race To: {raceTo} Balls
+            </p>
+
+            <img src="/divider.png" className="s-tracker-divider-css"></img>
+
+            <div className="s-player-container">
+                <div className="s-player1-container">
+                    <p id = "player1" className="s-player1-name">{player1}</p>
+                    <div className="s-score-container">
                         <button className="s-decrement-button">-</button>
-                        <p className="s-player1-score">0</p>
-                        <div className="s-increment-box">
+                        <p className="s-player1-score">{player1Score}</p>
+                        <div className="s-increment-container">
                             <button className="s-increment-button">+</button>
                             <button className="s-increment-button">+2</button>
-                            <div className="CR-box">
+                            <div className="CR-container">
                                 <button className="s-increment-button">CR</button>
                                 <button className="CR-icon">i</button>
                             </div>
@@ -40,29 +109,39 @@ const Tracker: React.FC = () => {
                     </div>
         
                     <div className="high-style player1-high-run">
-                        High Run: 0
+                        High Run: {player1HighRun}
                     </div>
                     <div className="high-style player1-curr-high-run">
-                        Current High Run: 0
+                        Current High Run: {player1CurrRun}
                     </div>
                 </div>
-        
-                
-                <div>
-                    <img src="/leftArrow.png" className="image-style" id="player-turn"></img>
-                    <div className="player-turn-text-style" id="player-turn-text">
+
+                <div className="remaining-turn-container">
+                    <div className="remaining-balls-container">
+                        <p className="remaining-balls-style remaining-balls">{remainingBalls}</p>
+                        <p className="remaining-balls-style">Remaining Balls</p>
+                        <p className="rack-number">(Rack {rack})</p>
                     </div>
-                    <div className="player-swap-text-style">
-                        (Press spacebar to swap turn)
+                    <p className="s-to-shoot-text"> 
+                        {toShoot} to shoot!
+                    </p>
+                    <div className="arrow-container">
+                        <img src={toShoot === player1 ? "/leftArrow.png" : "/rightArrow.png"} className="arrow-image-style" id="player-turn"></img>
+                        <div className="player-turn-text-style" id="player-turn-text">
+                        </div>
+                        <div className="player-swap-text-style">
+                            (Press spacebar to swap turn)
+                        </div>
                     </div>
+                    <button className="undo-style">Undo</button>
                 </div>
-                
-                <div className="s-player2-box">
-                    <p id = "player2" className="s-player2-name">John</p>
-                    <div className="s-score-box">
+
+                <div className="s-player2-container">
+                    <p id = "player2" className="s-player2-name">{player2}</p>
+                    <div className="s-score-container">
                         <button className="s-decrement-button">-</button>
-                        <p className="s-player2-score">0</p>
-                        <div className="s-increment-box">
+                        <p className="s-player2-score">{player2Score}</p>
+                        <div className="s-increment-container">
                             <button className="s-increment-button">+</button>
                             <button className="s-increment-button">+2</button>
                             <button className="s-increment-button">CR</button>
@@ -70,16 +149,12 @@ const Tracker: React.FC = () => {
                     </div>
 
                     <div className="high-style player2-high-run">
-                        High Run: 0
+                        High Run: {player2HighRun}
                     </div>
                     <div className="high-style player2-curr-high-run">
-                        Current High Run: 0
+                        Current High Run: {player2CurrRun}
                     </div>
                 </div>
-            </div>
-
-            <div className="undo-box">
-                <button className="undo-style">Undo</button>
             </div>
         </div>
 
