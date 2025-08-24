6a0a3468cc53a286c6109fb257ff5c8b35660e1f
Michael Tran <megadye123@gmail.com>
2025-08-23 23:33:04 -0500

    feat: inspector toggle

--- straight-tracker/next.config.ts
+++ straight-tracker/next.config.ts
@@ -2,10 +2,13 @@
 import type { NextConfig } from 'next';
 import { codeInspectorPlugin } from 'code-inspector-plugin';
 
+let useInspector = false;
+
 const nextConfig: NextConfig = {
   // Configure for both webpack and turbopack
+  
   webpack: (config, { dev, isServer }) => {
-    if (dev && !isServer) {
+    if (dev && !isServer && useInspector) {
       config.plugins.push(
         codeInspectorPlugin({
           bundler: 'webpack',
